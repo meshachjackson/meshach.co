bf076876c2bd03587f5e9a68eac9ed38b49f96e4
Meshach Jackson <meshach@deviumgames.com>
2021-01-06 11:55:33 -0800

    add theme

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,15 @@
     <b-jumbotron class="text-center">
       <h1>Coming soon.</h1>
     </b-jumbotron>
+    <nuxt-content :document="homePageContent" />
   </b-container>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const homePageContent = await $content("home").fetch();
+    return { homePageContent };
+  },
+};
+</script>
